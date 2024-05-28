f1e005caecc3f241176dbe5216d5e335b0a4c932
Sebastian <sebastian@flavet.se>
2024-05-28 22:50:44 +0200

    Changed transaction controller from Class

--- server/controllers/tx-controller.mjs
+++ server/controllers/tx-controller.mjs
@@ -1,39 +1,54 @@
 import Transaction from '../models/Transaction.mjs';
 
-class txController {
-  constructor() {
-    this.transactions = [];
+let transactions = [];
+
+export const createTx = (req, res, next) => {
+  const { amount, sender, recipient } = req.body;
+
+  if (!amount || !sender || !recipient) {
+    return res
+      .status(400)
+      .json({ error: 'Transaction details are incomplete' });
   }
 
-  createTx(details) {
-    if (!details.amount || !details.sender || !details.recipient) {
-      throw new Error('Transaction details are incomplete');
-    }
+  try {
+    const newTx = new Transaction({ amount, sender, recipient });
+    transactions.push(newTx);
+    res.status(201).json(newTx);
+  } catch (error) {
+    next(error);
+  }
+};
 
-    const newTx = new Transaction(details);
-    this.transactions.push(newTx);
-    return newTx;
+export const getTxById = (req, res, next) => {
+  const { txId } = req.params;
+
+  const transaction = transactions.find(
+    (transaction) => transaction.txId === txId
+  );
+
+  if (!transaction) {
+    return res.status(404).json({ error: 'Transaction not found' });
   }
 
-  getTx(txId) {
-    return this.transactions.find((tx) => tx.txId === txId);
+  res.status(200).json(transaction);
+};
+
+export const getAllTx = (req, res, next) => {
+  res.status(200).json(transactions);
+};
+
+export const deleteTxById = (req, res, next) => {
+  const { txId } = req.params;
+
+  const index = transactions.findIndex(
+    (transaction) => transaction.txId === txId
+  );
+
+  if (index === -1) {
+    return res.status(404).json({ error: 'Transaction not found' });
   }
 
-  getTxs() {
-    return this.transactions;
-  }
-
-  deleteTx(txId) {
-    const index = this.transactions.findIndex(
-      (transaction) => transaction.txId === txId
-    );
-    if (index === -1) {
-      throw new Error('Transaction not found');
-    }
-
-    const deletedTx = this.transactions.splice(index, 1);
-    return deletedTx;
-  }
-}
-
-export default txController;
+  const deletedTx = transactions.splice(index, 1);
+  res.status(200).json(deletedTx);
+};
